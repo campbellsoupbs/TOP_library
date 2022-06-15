a170381d4f5f6c666e9e7bb2108421092f281995
Brian Campbell <campbellsoupbs@gmail.com>
2022-06-15 15:08:23 -0400

    Create functions to convert modal form input to book object and add to array

--- scripts.js
+++ scripts.js
@@ -23,3 +23,51 @@ function closeModal() {
     modal.classList.remove('active');
     overlay.classList.remove('active');
 }
+
+// Books
+
+let myLibrary = [];     //Array
+let bookCount = 1;
+
+
+// Book constructor
+
+function Book(title, author, pages, isRead, count) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages,
+    this.isRead = isRead;
+    this.count = count;
+}
+
+// Add Book to Array
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+
+const form = document.querySelector("#addBookForm");
+
+form.addEventListener('submit', logInfo);
+
+function logInfo(e){
+    e.preventDefault();
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const checkbox = document.querySelector("#checkbox");
+    let isRead = "";
+    if (checkbox.checked) {
+        isRead = "yes";
+    } else {
+        isRead = "no";
+    }
+    let count = bookCount;
+
+    const newBook = new Book(title, author, pages, isRead, count);
+    addBookToLibrary(newBook);
+    console.log(myLibrary);
+    bookCount++;
+};
+
+
